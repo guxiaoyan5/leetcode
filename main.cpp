b26709a2b8b5a5d642bc1b99e2150a781a92aac7
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-04-25 19:01:37 +0800

    第3天第一次提交（15-20题）

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
 #include "algorithm"
 #include "iostream"
 #include "vector"
+#include "stack"
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            } else {
+                char d = st.top();
+                if ((s[i] == ')' && d == '(') || (s[i] == ']' && d == '[') || (s[i] == '}' && d == '{')) {
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
     }
 };
 
 int main() {
 
 }
-
