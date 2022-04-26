7f2b3b9584b581c25b9361ea0ce7e70507e4dc43
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-04-26 21:01:23 +0800

    第5天第一次提交（20-29题题）

--- main.cpp
+++ main.cpp
@@ -18,23 +18,11 @@ struct ListNode {
 
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> st;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-                st.push(s[i]);
-            } else {
-                char d = st.top();
-                if ((s[i] == ')' && d == '(') || (s[i] == ']' && d == '[') || (s[i] == '}' && d == '{')) {
-                    continue;
-                }else{
-                    return false;
-                }
-            }
-        }
-        return st.empty();
+    vector<int> findSubstring(string s, vector<string>& words) {
+
     }
 };
+
 
 int main() {
 
