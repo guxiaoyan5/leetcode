2be1be05021b82f09278eca2894b47bf2ec2e8e0
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-04-27 14:08:55 +0800

    第6天第一次提交（30题）

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+#include <unordered_map>
 #include "algorithm"
 #include "iostream"
 #include "vector"
-#include "stack"
+#include "map"
 
 using namespace std;
 
@@ -19,10 +20,40 @@ struct ListNode {
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string, int> allWordsMap;
+        for (auto& v : words) {
+            ++allWordsMap[v];
+        }
 
+        int num = words.size();
+        int onelen = words[0].length();
+        vector<int> res;
+        if (s.length() < num * onelen) {
+            return res;
+        }
+
+        for (int left = 0; left < s.length() - num * onelen + 1; ++left) {
+            unordered_map<string, int> nowWordsMap;
+            int right = left;
+            while (right < left + num * onelen) {
+                auto cur = s.substr(right, onelen);
+                if (allWordsMap.find(cur) == allWordsMap.end()
+                    || nowWordsMap[cur] == allWordsMap[cur]) {
+                    break;
+                }
+
+                ++nowWordsMap[cur];
+                right += onelen;
+            }
+
+            if (right == left + num * onelen) {
+                res.emplace_back(left);
+            }
+        }
+
+        return res;
     }
 };
-
 
 int main() {
 
