cb30c9cb0047c95bddf6c96ac2d105114913396b
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-05-04 18:10:33 +0800

    第6天第2次提交（35-40题）

--- main.cpp
+++ main.cpp
@@ -1,24 +1,57 @@
-#include <unordered_map>
 #include "algorithm"
 #include "iostream"
 #include "vector"
-#include "map"
 #include "stack"
+#include "map"
+#include "set"
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
+class Solution {
+public:
 
-    ListNode() : val(0), next(nullptr) {}
+    bool dfs(map<int, int> &m, vector<int> &key, vector<vector<int>> &results, vector<int> &result, int i, int s,
+             int target) {
+        if (s == target) {
+            results.push_back(result);
+            return false;
+        } else if (s > target) {
+            return false;
+        } else if (i >= m.size()) {
+            return true;
+        } else {
+            int j = 0;
+            int k = (target - s) / key[i];
+            bool flag = true;
+            for (; j < k + 1 && j < m[key[i]] && flag; ++j) {
+                flag = dfs(m, key, results, result, i + 1, s + j * key[i], target);
+                if (j != 0) result.push_back(key[i]);
+            }
+            for (int l = 1; l < j; ++l) {
+                result.pop_back();
+            }
+        }
+        return true;
+    }
 
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        vector<vector<int>> results;
+        vector<int> result;
+        map<int, int> m;
+        vector<int> key;
+        for (int i = 0; i < candidates.size(); ++i) {
+            if (m.count(candidates[i]) == 0) {
+                m[candidates[i]] = 1;
+                key.push_back(candidates[i]);
+            } else {
+                m[candidates[i]] += 1;
+            }
+        }
+        sort(key.begin(), key.end());
+        dfs(m,key,results,result,0,0,target);
+        return results;
+    }
 };
-
-
 
 
 int main() {
