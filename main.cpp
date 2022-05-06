d1e8bb3fdb8cb6e09ea315d2b0ba358c3fa9db44
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-05-06 15:46:19 +0800

    第7天第1次提交（41-43题）

--- main.cpp
+++ main.cpp
@@ -7,52 +7,13 @@
 
 using namespace std;
 
+
 class Solution {
 public:
+    bool isMatch(string s, string p) {
 
-    bool dfs(map<int, int> &m, vector<int> &key, vector<vector<int>> &results, vector<int> &result, int i, int s,
-             int target) {
-        if (s == target) {
-            results.push_back(result);
-            return false;
-        } else if (s > target) {
-            return false;
-        } else if (i >= m.size()) {
-            return true;
-        } else {
-            int j = 0;
-            int k = (target - s) / key[i];
-            bool flag = true;
-            for (; j < k + 1 && j < m[key[i]] && flag; ++j) {
-                flag = dfs(m, key, results, result, i + 1, s + j * key[i], target);
-                if (j != 0) result.push_back(key[i]);
-            }
-            for (int l = 1; l < j; ++l) {
-                result.pop_back();
-            }
-        }
-        return true;
-    }
-
-    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
-        vector<vector<int>> results;
-        vector<int> result;
-        map<int, int> m;
-        vector<int> key;
-        for (int i = 0; i < candidates.size(); ++i) {
-            if (m.count(candidates[i]) == 0) {
-                m[candidates[i]] = 1;
-                key.push_back(candidates[i]);
-            } else {
-                m[candidates[i]] += 1;
-            }
-        }
-        sort(key.begin(), key.end());
-        dfs(m,key,results,result,0,0,target);
-        return results;
     }
 };
-
 
 int main() {
 
