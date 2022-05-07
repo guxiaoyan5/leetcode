6c4d6273896e93a870befc0280d2e5fabe8c06af
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-05-07 17:19:54 +0800

    第9天第1次提交（46-48题）

--- main.cpp
+++ main.cpp
@@ -10,8 +10,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-
+    void rotate(vector<vector<int>> &matrix) {
+        if (matrix.size() == 1) return;
+        int n = matrix.size(), s = n % 2;
+        int temp;
+        for (int i = 0; i < s; ++i) {
+            for (int j = 0; j < n - 1 - 2 * i; ++j) {
+                temp = matrix[i][i+j];
+                matrix[i][i+j] = matrix[n-1-i-j][i];
+                matrix[n-1-i-j][i] = matrix[n-1-i][n-1-i-j];
+                matrix[n-1-i][n-1-i-j] = matrix[i+j][n-1-i];
+                matrix[i+j][n-1-i] = temp;
+            }
+        }
     }
 };
 
