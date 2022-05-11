654168eb560916b2d4de1c88b64d32535906fb89
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-05-11 17:20:19 +0800

    第11天第1次提交（53-60题）

--- main.cpp
+++ main.cpp
@@ -4,41 +4,20 @@
 #include "stack"
 #include "map"
 #include "set"
-
+#include <regex>
 using namespace std;
+
+bool camp(vector<int> &a, vector<int> &b) {
+    return a[0] < b[0];
+}
 
 class Solution {
 public:
-    void dfs(int i, int n, vector<bool> &visited,vector<bool> &left ,vector<bool> &right,vector<int> &queen, int &results) {
-        if (i == n) {
-            results+=1;
-            return;
-        }
-        for (int j = 0; j < n; ++j) {
-            if (!visited[j] &&!right[i+j] &&!left[n-i+j-1]) {
-                visited[j] = true;
-                right[i+j] = true;
-                left[n-i+j-1] = true;
-                queen[i] = j;
-                dfs(i+1, n, visited,left,right, queen, results);
-                visited[j] = false;
-                right[i+j] = false;
-                left[n-i+j-1] = false;
-                queen[i] = -1;
-            }
-        }
-    }
+    string getPermutation(int n, int k) {
 
-    int totalNQueens(int n) {
-        vector<bool> visited(n, false);
-        vector<bool> left(2 * n - 1, false);
-        vector<bool> right(2 * n - 1, false);
-        vector<int> queen(n, -1);
-        int results = 0;
-        dfs(0, n, visited,left,right, queen, results);
-        return results;
     }
 };
 
 int main() {
+
 }
