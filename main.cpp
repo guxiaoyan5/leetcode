18dc39f60beb88e07ec0fe6221475f8c2f8b85b0
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-07-14 21:46:10 +0800

    第13天第1次提交（71题）2022年7月14

--- main.cpp
+++ main.cpp
@@ -1,28 +1,16 @@
+#include <sstream>
 #include "iostream"
+#include "string"
 #include "vector"
-#include <regex>
-#include "deque"
 
 using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return n;
-        int temp1 = 1, temp2 = 2;
-        int temp;
-        int i = 3;
-        while (i <= n) {
-            temp = temp2 + temp1;
-            temp1 = temp2;
-            temp2 = temp;
-            i++;
-        }
-        return temp;
+    int minDistance(string word1, string word2) {
+
     }
 };
-
 int main() {
 
 }
