31c161d2228850a927164fd9020431724adcaaab
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-08-07 10:44:14 +0800

    第17天第2次提交2022年8月7号

--- main.cpp
+++ main.cpp
@@ -11,23 +11,10 @@ using namespace std;
 
 class Solution {
 public:
-    void setZeroes(vector<vector<int>> &matrix) {
-        vector<bool> row(matrix.size(), false);
-        vector<bool> col(matrix[0].size(), false);
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
-                if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
-                }
-            }
-        }
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
-                }
-            }
+    int removeDuplicates(vector<int> &nums) {
+        int flag, i, s;
+        for (int j = 0; j < nums.size(); ++j) {
+
         }
     }
 };
