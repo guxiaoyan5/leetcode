d293dc2c41a9a37c7bfa0b2811a45e1e24471211
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-08-11 16:57:59 +0800

    第18天第1次提交2022年8月11号

--- main.cpp
+++ main.cpp
@@ -5,16 +5,30 @@
 #include "vector"
 #include "queue"
 #include <algorithm>
+#include <unordered_set>
+#include <set>
 
 using namespace std;
 
 
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+    ListNode *insertionSortList(ListNode *head) {
 
     }
 };
+
 int main() {
 
 }
