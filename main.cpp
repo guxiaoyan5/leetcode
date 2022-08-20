85fb693a3355ab4dcde90ba42f02a81e205872a8
guxiaoyan5 <49086964+guxiaoyan5@users.noreply.github.com>
2022-08-20 19:12:34 +0800

    第19天第1次提交2022年8月20号

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <unordered_set>
 #include <set>
+#include "map"
 
 using namespace std;
 
@@ -22,10 +23,38 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
 class Solution {
 public:
-    ListNode *insertionSortList(ListNode *head) {
+    TreeNode *dfs(vector<int> &nums, int low, int high) {
+        if (low > high) return nullptr;
+        if (low == high) return new TreeNode(nums[low]);
+        int mid = (low + high) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = dfs(nums, low, mid - 1);
+        root->right = dfs(nums, mid + 1, high);
+        return root;
+    }
 
+    TreeNode *sortedListToBST(ListNode *head) {
+        vector<int> nums;
+        while (head) {
+            nums.push_back(head->val);
+            head = head->next;
+        }
+        return dfs(nums, 0, nums.size() - 1);
     }
 };
 
